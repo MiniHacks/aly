a3f5e1ca31e854875b0eb0ecd9a8b8415fd90e33
Ansh Patel <pate1011@umn.edu>
2023-02-18 18:49:32 -0500

    Next Up Location Icon 😇

--- packages/mobile/components/NextUpButton.js
+++ packages/mobile/components/NextUpButton.js
@@ -1,44 +1,76 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-
-export default function StrikeButton({ strikeCount }) {
-  const text = ["on track.", "Strike 1", "Strike 2", "Strike 3"][strikeCount];
+import { Entypo } from "@expo/vector-icons";
+export default function NextUpButton({ event }) {
+  const text = ["be at MetroTech.", "be at 2041", "be at 2033", "Go to gym"][
+    event
+  ];
   return (
-    <View
-      style={{
-        flex: 1,
-        dropShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-        borderRadius: 10,
-        width: "85%",
-        overflow: "hidden",
-      }}
-    >
+    <View style={styles.bigbox}>
+      <View
+        style={{
+          backgroundColor: "#FFD7EF",
+          borderRadius: 10,
+          height: 40,
+          width: 40,
+          marginTop: 10,
+          overflow: "hidden",
+          marginLeft: 10,
+        }}
+      >
+        <Entypo
+          style={{
+            marginLeft: 5,
+            marginTop: 5,
+          }}
+          name="location-pin"
+          size={30}
+          color="#A42A73"
+        />
+      </View>
       <View style={styles.box1}>
         <Text style={styles.text1}>{text}</Text>
+        <Text style={styles.text2}>by 11:00 AM</Text>
       </View>
     </View>
   );
 }
 const styles = StyleSheet.create({
   text1: {
-    fontVariant: "bold",
-    color: "white",
+    color: "#3D3A9E",
     fontFamily: "Poppins",
-    fontStyle: "normal",
-    fontWeight: "700",
-    fontSize: 40,
-    lineHeight: 60,
+    fontStyle: "semi-bold",
+    fontSize: 19,
+    lineHeight: 20,
+    paddingLeft: 10,
     display: "flex",
-    alignItems: "center",
-    textAlign: "center",
+  },
+  text2: {
+    fontSize: 12,
+    color: "black",
+    fontFamily: "Poppins",
+    fontStyle: "light",
+    paddingLeft: 10,
   },
   box1: {
     flex: 1,
     flexDirection: "column",
     justifyContent: "center",
-    alignItems: "center",
     backgroundColor: "#FFF",
     height: 60,
+  },
+  bigbox: {
+    flex: 1,
+    flexDirection: "row",
     dropShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+    borderRadius: 10,
+    backgroundColor: "#FFF",
+    width: "85%",
+    overflow: "hidden",
+    maxHeight: 60,
+    shadowColor: "#171717",
+    shadowOffset: { width: -2, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
   },
 });
