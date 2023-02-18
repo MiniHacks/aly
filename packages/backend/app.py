e8963819be68577d8d470b20e44ce4bd8419d011
Samyok Nepal <sam@yok.dev>
2023-02-18 17:20:07 -0500

    🥹 cors

--- packages/backend/app.py
+++ packages/backend/app.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
 from typing import Union
 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class WebsiteText(BaseModel):
