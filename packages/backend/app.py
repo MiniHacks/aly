e93f3cb7f9841c937007eb895fd08c4dde5639bb
Samyok Nepal <sam@yok.dev>
2023-02-18 17:02:56 -0500

    🥹 backend + chrome

--- packages/backend/app.py
+++ packages/backend/app.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class WebsiteText(BaseModel):
+    heading: str | None
+    title: str | None
+    body: str | None
+    userId: str
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+async def web_analysis(website: WebsiteText):
+    # do some analysis
+    return {"message": "woah"}
--- packages/chrome/injection/injection.js
+++ packages/chrome/injection/injection.js
@@ -1,1 +1,20 @@
+const BASE_URL = "http://localhost:5001/simil";
+
 console.log("injection.js loaded");
+(function () {
+  fetch(`${BASE_URL}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title: document.querySelector("title")?.innerText,
+      header: document.querySelector("h1")?.innerText,
+      url: window.location.href,
+    }),
+  })
+    .then((r) => r.json())
+    .then((r) => {
+      console.log(r);
+    });
+})();
