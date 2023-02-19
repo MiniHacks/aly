9b5250844477e731c76229d7c7309961e3f79ae1
Brandon Lam <lam00107@umn.edu>
2023-02-19 06:05:21 -0500

    🤩we have a MAP

--- packages/mobile/screens/BeSomewhereScreen.js
+++ packages/mobile/screens/BeSomewhereScreen.js
@@ -1,10 +1,64 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import MapView from "react-native-maps";
 
-export default function BeSomeWhereScreen(navigation) {
+import Marker from "../components/marker.png";
+
+export default function BeSomewhereScreen(navigation) {
+  const [region, setRegion] = React.useState({
+    latitude: 40.6942999,
+    longitude: -73.9867675,
+  });
+
   return (
-    <View>
-      <Text>Be Somewhere</Text>
+    <View style={styles.map}>
+      <MapView
+        initialRegion={{
+          latitude: 40.6942999,
+          longitude: -73.9867675,
+          latitudeDelta: 0.0015,
+          longitudeDelta: 0.0015,
+        }}
+        onRegionChangeComplete={setRegion}
+        style={styles.map}
+      />
+      <View style={styles.markerFixed}>
+        <Image style={styles.marker} source={Marker} />
+      </View>
+      <SafeAreaView style={styles.footer}>
+        <Text style={styles.region}>
+          Coords: {region.longitude}, {region.latitude}
+        </Text>
+      </SafeAreaView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  map: {
+    height: "100%",
+    width: "100%",
+  },
+  markerFixed: {
+    left: "50%",
+    marginLeft: -24,
+    marginTop: -48,
+    position: "absolute",
+    top: "50%",
+  },
+  marker: {
+    height: 48,
+    width: 48,
+  },
+  footer: {
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    bottom: 0,
+    position: "absolute",
+    width: "100%",
+  },
+  region: {
+    color: "#fff",
+    lineHeight: 20,
+    margin: 20,
+  },
+});
