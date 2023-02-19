0889a87529bc614aa52c5f62ecc9a368bf8e0f43
Ansh Patel <pate1011@umn.edu>
2023-02-19 04:36:13 -0500

    🦵🏾do something UI

--- packages/mobile/screens/DoSomethingScreen.js
+++ packages/mobile/screens/DoSomethingScreen.js
@@ -1,34 +1,90 @@
 import React, { useState } from "react";
-import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+import {
+  Image,
+  ImageBackground,
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import DatePicker from "react-native-date-picker";
+import TaskBG from "../components/TaskBG.png";
 
 export default function DoSomething(navigation) {
   const [date, setDate] = useState(new Date());
   const [text, onChangeText] = React.useState("");
 
   return (
-    <SafeAreaView
-      style={{
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
-        placeholder={"What task do you want to do?"}
-      />
-      <DatePicker date={date} onDateChange={setDate} />
-    </SafeAreaView>
+    <ImageBackground source={TaskBG} style={styles.image}>
+      <Text
+        style={{
+          color: "#fff",
+          fontSize: 30,
+          fontFamily: "Poppins_600SemiBold",
+          textAlign: "center",
+        }}
+      >
+        do something
+      </Text>
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          marginTop: 20,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+          placeholder={"enter task here"}
+          placeholderTextColor={"#2D3748"}
+        />
+        <DatePicker date={date} onDateChange={setDate} />
+      </View>
+      <Pressable
+        style={{
+          backgroundColor: "#fff",
+          width: 60,
+          height: 40,
+          borderRadius: 20,
+          marginLeft: "7.5%",
+          overflow: "hidden",
+        }}
+      >
+        <Text
+          style={{
+            color: "#2D3748",
+            fontSize: 15,
+            fontFamily: "Poppins_600SemiBold",
+            textAlign: "center",
+            marginTop: 10,
+          }}
+        ></Text>
+      </Pressable>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
+    height: 50,
+    maxWidth: "85%",
+    minWidth: "85%",
+    marginHorizontal: 12,
+    borderRadius: 10,
+    borderColor: "white",
+    backgroundColor: "white",
+    color: "black",
     padding: 10,
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
   },
 });
