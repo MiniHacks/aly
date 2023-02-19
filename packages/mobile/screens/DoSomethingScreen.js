90494c9ee35c61758683bb716f90f2be7f36aa1e
Brandon Lam <lam00107@umn.edu>
2023-02-19 03:33:59 -0500

    🤣dosomething components need to style + return them

--- packages/mobile/screens/DoSomethingScreen.js
+++ packages/mobile/screens/DoSomethingScreen.js
@@ -1,10 +1,29 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+import DatePicker from "react-native-date-picker";
 
-export default function DoSomethingScreen(navigation) {
+export default function DoSomething(navigation) {
+  const [date, setDate] = useState(new Date());
+  const [text, onChangeText] = React.useState("");
+
   return (
-    <View>
-      <Text>Do Something</Text>
-    </View>
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder={"What task do you want to do?"}
+      />
+      <DatePicker date={date} onDateChange={setDate} />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
